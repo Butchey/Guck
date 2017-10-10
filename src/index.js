6e0aa088dddaa6c4cb69f225e76021cb256267ce
Philipp Fleischer <privat@butchey.me>
2017-10-10 23:39:45 +0200

    Idk :(

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // Initiate Datastores
-const Datastore = require('nedb');
+//const config = require('/config');
 
-db = {};
-db.users = new Datastore('db/users.db');
+import User from './model/user/index';
+
+var user = new User;
+user.create({ name: 'Butchey', created: new Date(), password: 'testpass123', mail: 'privat@butchey.me' });
+/*
 db.movies = new Datastore('db/movies.db');
 db.audio = new Datastore('db/audio.db');
 db.ebooks = new Datastore('db/ebooks.db');
+*/
 
 
 
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
