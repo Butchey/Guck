4e7ffb5f5a0f832574156785ebe392c751d4f48c
Philipp Fleischer <privat@butchey.me>
2017-12-05 14:12:07 +0100

    added medialib Schemes

---
+++ src/api/medialib/modules/movie/model.js
@@ -0,0 +1,22 @@
+// grab the things we need
+import mongoose from 'mongoose';
+var Schema = mongoose.Schema;
+
+// create a schema
+var movieSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  admin: { type: Boolean, default: false }
+},
+{
+    timestamps: true
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var User = mongoose.model('User', userSchema);
+
+// make this available to our users in our Node applications
+module.exports = User;
