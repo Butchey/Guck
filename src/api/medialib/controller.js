1536dc0dfbad17062ee82c366793f23fa1016ebe
Philipp Fleischer <privat@butchey.me>
2017-12-09 21:46:15 +0100

    added medialib Controller

---
+++ src/api/medialib/controller.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import Medialib from './model';
+
+function get(req, res) {
+}
+
+function update(req, res) {
+}
+
+function create(req, res, next) {
+    let lib = new Medialib(req.body);
+    const module = require('./modules/' + req.body.module + '/model');
+    lib.content = new module();
+    lib.save(( err, dbres ) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(dbres);
+        }
+      });
+}
+
+function list(req, res) {
+    Medialib.find((err, dbres) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(dbres);
+        }
+      });
+}
+
+function remove(req, res, next) {
+    Medialib.remove( req.body, (err, dbres) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(dbres);
+        }
+      });
+}
+
+export default { get, update, create, list, remove };
