569cfbb2e4665de05bed147b78f2c34d953122d6
Philipp Fleischer <privat@butchey.me>
2017-12-09 21:44:45 +0100

    fixed and extended medialibSchema

--- src/api/medialib/model.js
+++ src/api/medialib/model.js
@@ -1,22 +1,20 @@
-// grab the things we need
 import mongoose from 'mongoose';
 var Schema = mongoose.Schema;
 
-// create a schema
+
 var medialibSchema = new Schema({
-  name: String,
-  module: { type: String, required: true, unique: true }, //    type: module
-  locations: { type: Array },
-  agent: { type: String }, //    type: agent
-  scanner: { type: String } //    type: scanner
+  name: { type: String, required: true },
+  module: { type: String, required: true },
+  content: { type: Schema.Types.Mixed, unique: true },
+  locations: { type: Array, required: true },
+  agent: { type: String },
+  scanner: { type: String }
 },
 {
     timestamps: true
 });
 
-// the schema is useless so far
-// we need to create a model using it
-var User = mongoose.model('Medialib', medialibSchema);
+var Medialib = mongoose.model('Medialib', medialibSchema);
 
-// make this available to our users in our Node applications
-module.exports = User;
+
+module.exports = Medialib;
